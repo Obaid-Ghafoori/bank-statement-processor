b0c8a9aa83bace6032fee71695d82956f527d098
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-09-23 23:55:28 +0200

    Initial commit: Added bank statement processor project

---
+++ src/main/java/dev/bankstatement/fileprocessing/BankStatementFileProcessingApplication.java
@@ -0,0 +1,13 @@
+package dev.bankstatement.fileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankStatementFileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankStatementFileProcessingApplication.class, args);
+	}
+
+}
