9197b13d6ea0c8ef40d5134260eb705915af1694
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-09-27 00:29:46 +0200

    chor: precess pr feedback

--- src/test/java/dev/bankstatement/fileprocessing/service/UserServiceTest.java
+++ src/test/java/dev/bankstatement/fileprocessing/service/UserServiceTest.java
@@ -97,12 +97,12 @@ class UserServiceTest {
 
         when(userRepository.findByUsername("Tom")).thenReturn(Optional.of(existingUser));  // User already exists
 
-        // Act & Assert using AssertJ
+        // Act & Assert
         assertThatThrownBy(() -> userService.registerUser("Tom", "1234ojd", "ROLE_USER"))
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("User already exists with username: Tom");
 
-        // Verify userRepository.save() is never called since the user already exists
+        // Verify
         verify(userRepository, never()).save(any(User.class));
     }
 
