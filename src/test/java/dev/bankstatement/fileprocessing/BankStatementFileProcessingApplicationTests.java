e1062c0eafb54872dad6182ba86512fdd2bcad7b
Obaid Ghafoori <obaid.ghafoori9@gmail.com>
2024-09-28 22:47:52 +0200

    feat: add BankStatementFileProcessingApplication class to test tag

--- src/test/java/dev/bankstatement/fileprocessing/BankStatementFileProcessingApplicationTests.java
+++ src/test/java/dev/bankstatement/fileprocessing/BankStatementFileProcessingApplicationTests.java
@@ -3,7 +3,7 @@ package dev.bankstatement.fileprocessing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = BankStatementFileProcessingApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BankStatementFileProcessingApplicationTests {
 
 	@Test
